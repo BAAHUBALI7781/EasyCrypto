4a39caa8282048de311fd98000787ba3f60ba52a
Divyansh <divyanshm1609@gmail.com>
2021-12-08 21:46:30 +0530

    Router path

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV === 'production'){
   app.use(express.static("client/build"));
 }
 
-app.use('/',require('./router/index'));
+app.use(require('./router/index'));
 app.listen(port,function(err){
     console.log("Server listening");
 })
